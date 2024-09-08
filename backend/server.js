ccd77ae210737ac9e0a635094537c268883359f3
Yashodha Jayasinghe <yashodhalasithjayasinghe@gmail.com>
2024-09-08 17:20:07 +0530

    Connected to data base

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,29 @@
 // server.js
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config(); // This loads environment variables
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
+const URL = process.env.MONGODB_URL;
 
 // Example route
-app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
+app.get("/", (req, res) => {
+  res.send("Hello from the backend!");
 });
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+mongoose
+  .connect(URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((error) => console.log(error.message));
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Mongodb Connection Success!");
 });
