bb51829b422d79495f1c1b568cc69ddba6510201
LiyanageRS <IT22366368@my.sliit.lk>
2024-09-14 14:07:10 +0530

    new changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,26 @@ import { Button } from "@material-tailwind/react";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import AddProduct from "./pages/AddProduct";
 import { NavigationBar } from "./components/NavigationBar";
+import ViewProducts from "./pages/ViewProducts";
+import UpdateProduct from './pages/UpdateProduct';
+import ProductDetail from "./pages/ProductDetail";
+import { Footer } from "./components/Footer";
+
 
 export default function App() {
   return (
     <BrowserRouter>
-      <NavigationBar />
+      <NavigationBar /> 
       <Routes>
         <Route path="/home" element={<Home />} />
+        <Route path="/add-product" element={<AddProduct />} />
+        <Route path="/products" element={<ViewProducts />} />
+        <Route path="/update-product/:id" element={<UpdateProduct />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
