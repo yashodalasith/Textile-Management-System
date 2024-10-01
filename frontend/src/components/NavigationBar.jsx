d7a1c3404cab588485c57ecda056b7c9b00dc9ea
Sayun Hettiarachchi <154741639+SayunHetti@users.noreply.github.com>
2024-10-01 10:53:50 +0530

    Name change Header

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -176,7 +176,7 @@ export function NavigationBar() {
             variant="h6"
             className="mr-4 cursor-pointer py-1.5 lg:ml-2"
           >
-            Home
+            Omusono
           </Typography>
         </Link>
 
